baa50493cd4564fcf06705fcc54d543744ceb7ea
Sergio Nezhigay <abc1971h@gmail.com>
2023-03-12 13:33:53 +0200

    refactored

--- js/main.js
+++ js/main.js
@@ -1,25 +1,20 @@
-const a = +prompt(`Enter, please, your "A" value (should not be negative)...`);
-const b = +prompt(
-  `Enter, please, your "B" value ("B" should be greater than "A")...`
-);
-const h = +prompt(
-  `Enter, please, your 'H' value ("H" should be greater than 0)...`
-);
+const a = +prompt(`Enter a non-negative "A" value:`);
+const b = +prompt(`Enter a "B" value greater than "A":`);
+const h = +prompt(`Enter a positive "H" value:`);
 
 let sumOfFactorials = 0;
 
-if (b > a) {
-  if (h > 0 && a >= 0 && b > 0) {
-    for (let i = a; i <= b; i += h) {
-      let res = 1;
-      for (j = 1; j <= i; j++) {
-        res *= j;
-      }
-      sumOfFactorials += res;
+if (b <= a) {
+  alert(`"B" should be greater than "A"!`);
+} else if (h <= 0 || a < 0 || b < 0) {
+  alert(`"A" should be non-negative, "B" and "H" should be positive!`);
+} else {
+  for (let i = a; i <= b; i += h) {
+    let res = 1;
+    for (j = 1; j <= i; j++) {
+      res *= j;
     }
-    alert(`Sum of factorial as to task is ${sumOfFactorials} ! Hooray!`);
-  } else
-    alert(
-      `B and H should be greater than 0, A should not be negative! Please, try one more time... :(`
-    );
-} else alert(`"B" should be greater than "A"! :(`);
+    sumOfFactorials += res;
+  }
+  alert(`The sum of factorials is ${sumOfFactorials}!`);
+}
